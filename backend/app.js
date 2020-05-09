02fc96d1c1be6edc99260c7387c173e77db51214
karan <rawatkaran930@gmail.com>
2020-05-09 15:15:09 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors= require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+mongoose.connect('mongodb://localhost/NIFTY',{ useNewUrlParser: true ,useUnifiedTopology: true },(err)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else{
+        console.log('database connected')
+    }
+})
+
+let niftyschema= new mongoose.Schema({
+    Open:Number,
+    High:Number,
+    Low:Number,
+    Close:Number,
+Time:Date
+
+})
+
+let DataNifty = mongoose.model('DataNifty',niftyschema);
+
+
+
+app.get('/data',(req,res)=>{
+    DataNifty.find({},(err,d)=>{
+        if(err)
+        {
+            console.log(err)
+        } 
+        else{
+      
+         res.send(d);
+        }
+    })
+   
+})
+
+
+
+app.listen(5000,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    else{
+        console.log('server connected')
+    }
+})
